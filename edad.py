f6a23e21ce2f3b4f62433d27ca3754258734703a
alghe2 <al.simpertigue@duocuc.cl>
2025-05-08 00:21:45 -0400

    pre_prueba_ale

---
+++ edad.py
@@ -0,0 +1,15 @@
+nombre = input("Bienvenido, cual es tu nombre?: ")
+print("Bienvenido ", nombre)
+
+edad = int(input("Cuantos años tienes?: "))
+
+if edad < 100:
+    años_faltante = 100 - edad
+    print(f"Te faltan {años_faltante} para llegar a 100 años :)")
+
+elif edad == 100:
+    print("Felicidades ya tienes 100 años! :)")
+
+else:  
+    print("Felicidades pasaste los 100 años! :)")
+
